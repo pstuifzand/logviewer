1ab7d7020dd927ce8b81fd4f256d2dfe4f6f48f9
Peter Stuifzand <peter@stuifzand.eu>
2015-07-07 07:45:41 +0200

    Use uuid for log message

--- bin/index.pl
+++ bin/index.pl
@@ -4,19 +4,21 @@ use WebService::Solr;
 use DateTime;
 use JSON::XS 'decode_json';
 use Data::Dumper;
+use Data::UUID;
 
 my $io = IO::Socket::IP->new(Proto => 'udp', LocalPort => '5066');
 
 my $solr = WebService::Solr->new('http://localhost:8983/solr/logviewer');
 
-my $id = 4;
+my $ug = Data::UUID->new;
+
 my $data;
 while ($io->recv($data, 4096)) {
     my $o = decode_json($data);
     print Dumper($o);
 
     my $doc = {
-        id => $id++,
+        id => $ug->create_str,
         date_i => delete $o->{date},
         message_txt => delete $o->{message},
         filename_s => delete $o->{filename},
--- cpanfile
+++ cpanfile
@@ -2,3 +2,4 @@ requires 'Dancer2';
 requires 'DBIx::Class';
 requires 'WebService::Solr';
 requires 'DBIx::DWIW';
+requires 'Data::UUID';
